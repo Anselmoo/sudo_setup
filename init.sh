2b7c13dddb80f2e1da1c20287038ec2f6b1415d8
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-04-19 11:13:27 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo apt-get -y upgrade
 sudo apt -y install build-essential
 sudo apt -y install cmake
 sudo apt-get -y install python3-dev
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 sudo apt -y install zsh
